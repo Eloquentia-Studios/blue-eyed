1af86de4c0a02e9c37d1a0729d4f02af027449f3
Esaias Cronelius <esaias@cronelius.se>
2023-06-28 21:10:28 +0200

    Set token cookie.
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>

--- backend/src/routers/authorizationRouter.ts
+++ backend/src/routers/authorizationRouter.ts
@@ -11,11 +11,11 @@ export const authorizationRouter = t.router({
         password: z.string()
       })
     )
-    .mutation(async ({ input }) => {
+    .mutation(async ({ input, ctx: { res } }) => {
       const valid = checkCredentials(input.username, input.password)
       if (!valid) throw new TRPCError({ message: 'Invalid username and/or password.', code: 'UNAUTHORIZED' })
 
       const token = await createAuthorizedToken(input.username)
-      return { token }
+      res.set('Set-Cookie', `blue-eyed-token=${token}; Path=/; HttpOnly; SameSite=Strict; Secure`)
     })
 })
