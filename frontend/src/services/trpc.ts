92175b77d2214da2090ed8dd6d96b5d01926fcd5
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2023-07-02 19:38:47 +0200

    Added svelte query and rewrote unauthorized layout

--- frontend/src/services/trpc.ts
+++ frontend/src/services/trpc.ts
@@ -1,10 +1,18 @@
+import type { QueryClient } from '@tanstack/svelte-query'
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
 import type { inferRouterOutputs } from '@trpc/server'
+import { svelteQueryWrapper } from 'trpc-svelte-query-adapter'
 import type { AppRouter } from '../../../backend/src/routers/appRouter'
 
-const trpc = createTRPCProxyClient<AppRouter>({
+const client = createTRPCProxyClient<AppRouter>({
   links: [httpBatchLink({ url: '/trpc' })]
 })
+
+const trpc = (queryClient?: QueryClient) =>
+  svelteQueryWrapper<AppRouter>({
+    client,
+    queryClient
+  })
 
 export default trpc
 
