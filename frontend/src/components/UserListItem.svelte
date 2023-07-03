f4a75a36e7758dc4a92c815d67af845fca5958eb
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2023-07-03 12:02:00 +0200

    Added reset dialog

--- frontend/src/components/UserListItem.svelte
+++ frontend/src/components/UserListItem.svelte
@@ -3,33 +3,41 @@
   import trpc from '../services/trpc'
   import { useDeleteUser } from '../services/user'
   import ConfirmDialog from './ConfirmDialog.svelte'
+  import CopyField from './CopyField.svelte'
   import IconButton from './IconButton.svelte'
   import IconTextButton from './IconTextButton.svelte'
   import MenuButton from './MenuButton.svelte'
+  import Modal from './Modal.svelte'
   import PopoutMenu from './PopoutMenu.svelte'
 
   const deleteUserMutation = useDeleteUser()
   const requestPasswordReset = trpc().requestPasswordReset.createMutation()
 
+  export let user: RouterOutput['getUsers'][number]
+
   let isMenuOpen = false
   const toggleMenu = () => (isMenuOpen = !isMenuOpen)
 
-  export let user: RouterOutput['getUsers'][number]
-
+  let resetPasswordOpen = false
+  let resetLink = ''
   const resetPassword = async () => {
     const resetToken = await $requestPasswordReset.mutateAsync(user.id)
-    const url = `${window.location.origin}/reset?resetToken=${encodeURIComponent(resetToken)}`
-    window.prompt('Copy to clipboard: Ctrl+C, Enter', url)
+    resetLink = `${window.location.origin}/reset?resetToken=${encodeURIComponent(resetToken)}`
+    resetPasswordOpen = true
   }
 
   let deleteOpen = false
-
   const openDelete = () => (deleteOpen = true)
-
   const deleteUser = async () => $deleteUserMutation.mutate(user.id)
 </script>
 
 <ConfirmDialog confirm={deleteUser} title="Are you sure you want to delete {user.displayName}?" bind:open={deleteOpen} destructive />
+
+<Modal bind:open={resetPasswordOpen}>
+  <h4 class="text-lg font-bold">Reset link for {user.displayName}</h4>
+  <p class="text-sm text-gray-400">The link is automatically copied to your clipboard.</p>
+  <CopyField value={resetLink} copyOnMount />
+</Modal>
 
 <div class="relative flex items-center justify-between p-2 pb-3 pt-3 after:absolute after:left-[5%] after:w-[90%] after:h-[1px] after:bottom-0 after:bg-gray-800 after:block last:after:hidden">
   <div class="flex flex-col sm:flex-row sm:gap-3">
