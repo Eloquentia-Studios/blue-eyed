a58c1ce01dc2a4c0a53a020364c1ebef7c5e883d
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2023-07-04 10:03:30 +0200

    Fixed bug with registration fields not showing errors

--- frontend/src/routes/(unauthorized)/register/+page.svelte
+++ frontend/src/routes/(unauthorized)/register/+page.svelte
@@ -44,8 +44,8 @@
 
   <ErrorMessage {errorMessage} />
 
-  <Input type="text" label="Username" bind:value={username} error={invalidFields['username']} />
-  <Input type="email" label="Email" bind:value={email} error={invalidFields['email']} />
-  <Input type="password" label="Password" bind:value={password} error={invalidFields['password']} />
+  <Input type="text" label="Username" bind:value={username} error={invalidFields['registrationInfo.username']} />
+  <Input type="email" label="Email" bind:value={email} error={invalidFields['registrationInfo.email']} />
+  <Input type="password" label="Password" bind:value={password} error={invalidFields['registrationInfo.password']} />
   <Button type="submit" class="mt-2">Create Account</Button>
 </CenteredContainerForm>
