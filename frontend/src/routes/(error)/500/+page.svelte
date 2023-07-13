5702c0949612afbe8ac8d5434a601ebb02def0e6
Esaias Cronelius <esaias@cronelius.se>
2023-07-13 21:18:59 +0200

    Redesigned 500 error page
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>

--- frontend/src/routes/(error)/500/+page.svelte
+++ frontend/src/routes/(error)/500/+page.svelte
@@ -1,1 +1,5 @@
-500 Error
+<div class="flex flex-col items-center justify-center w-screen h-screen gap-4 text-center">
+  <span class="font-bold text-9xl text-primary-500">500</span>
+  <span class="text-3xl font-semibold text-primary-50">Internal Server Error</span>
+  <span class="text-xl font-semibold text-primary-50">We were too Blue Eyed to foresee this situation...</span>
+</div>
