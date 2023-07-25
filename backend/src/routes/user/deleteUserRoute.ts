e8a9fb2b59d6b22f2a25f4a8d534d3f77fd27edb
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2023-07-25 13:46:59 +0200

    Protect route to delete user

--- backend/src/routes/user/deleteUserRoute.ts
+++ backend/src/routes/user/deleteUserRoute.ts
@@ -1,13 +1,15 @@
 import { z } from 'zod'
-import authenticatedProcedure from '../../procedures/authenticatedProcedure'
+import permissionProcedure from '../../procedures/permissionProcedure'
 import logger from '../../services/logging'
 import { deleteUser } from '../../services/user'
 
-const deleteUserRoute = authenticatedProcedure.input(z.string()).mutation(async ({ input: userId }) => {
-  logger.verbose(`Trying to delete user with id: ${userId}`)
+const deleteUserRoute = permissionProcedure(['DELETE_USER'])
+  .input(z.string())
+  .mutation(async ({ input: userId }) => {
+    logger.verbose(`Trying to delete user with id: ${userId}`)
 
-  const user = await deleteUser(userId)
-  logger.verbose(`User ${user.displayName} (${userId}) deleted`)
-})
+    const user = await deleteUser(userId)
+    logger.verbose(`User ${user.displayName} (${userId}) deleted`)
+  })
 
 export default deleteUserRoute
