a8b512cfd0b640807d2aca15b6e7c22c7fbd04f5
Esaias Cronelius <esaias@cronelius.se>
2023-08-01 13:54:33 +0200

    Drawer desktop styling
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>

--- frontend/src/components/Drawer.svelte
+++ frontend/src/components/Drawer.svelte
@@ -7,7 +7,7 @@
 </script>
 
 {#if open}
-  <div transition:slide class="fixed bottom-0 left-0 right-0 p-4 overflow-y-auto max-h-[80%] bg-slate-900 rounded-t-lg z-20">
+  <div transition:slide class="fixed bottom-0 left-0 right-0 p-4 overflow-y-auto max-h-[80%] bg-slate-900 rounded-t-lg z-20 md:max-w-md md:mx-auto md:top-0 md:bottom-auto md:rounded-t-none md:rounded-b-lg">
     <slot />
   </div>
 
