1ee6e2fff8fadce5772857cd1d1af5d087773e12
Esaias Cronelius <esaias@cronelius.se>
2023-08-23 14:25:12 +0200

    Require permission from higher role when changing lower role permissions
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>

--- backend/src/routes/role/changeRolePermissionRoute.ts
+++ backend/src/routes/role/changeRolePermissionRoute.ts
@@ -1,7 +1,9 @@
 import type { Permission } from '@prisma/client'
 import { z } from 'zod'
+import throwAndLogTRPCError from '../../libs/throwAndLogTRPCError'
 import rolePermissionProcedure from '../../procedures/rolePermissionProcedure'
-import { allPermissions, changeRolePermission } from '../../services/permission'
+import { allPermissions, changeRolePermission, getHighestUserRoleWithPermissions } from '../../services/permission'
+import { roleIsAboveOtherRole } from '../../services/role'
 
 const changeRolePermissionRoute = rolePermissionProcedure(['ROLES_WRITE'])
   .input(
@@ -14,6 +16,14 @@ const changeRolePermissionRoute = rolePermissionProcedure(['ROLES_WRITE'])
       enabled: z.boolean()
     })
   )
-  .mutation(async ({ input: { targetedRoleId, permission, enabled } }) => await changeRolePermission(targetedRoleId, permission, enabled))
+  .mutation(async ({ ctx, input: { targetedRoleId, permission, enabled } }) => {
+    const highestUserRole = await getHighestUserRoleWithPermissions(ctx.user.id, [permission])
+    if (!highestUserRole) return throwAndLogTRPCError('FORBIDDEN', 'forbidden', `User ${ctx.user.username} tried to change a role permission they don't have.`, 'warn')
+
+    const userRoleIsHigher = await roleIsAboveOtherRole(highestUserRole.id, targetedRoleId)
+    if (!userRoleIsHigher) return throwAndLogTRPCError('FORBIDDEN', 'forbidden', `User ${ctx.user.username} tried to change a role permission for a role which is above or the same as their highest role.`, 'warn')
+
+    return await changeRolePermission(targetedRoleId, permission, enabled)
+  })
 
 export default changeRolePermissionRoute
